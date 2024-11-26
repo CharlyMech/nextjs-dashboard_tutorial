352a1d4943f1e52a6bf0f1fc33e85859cd9772a6
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-11-26 17:04:03 +0100

    feat: chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,30 +17,27 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-	// ? This is a way that might be useful when form has many fields:
-	// const rawFormData = Object.fromEntries(formData.entries());
-	/* const rawFormData = {
-		customerId: formData.get("customerId"),
-		amount: formData.get("amount"),
-		status: formData.get("status"),
-	}; */
-	// With ZOD validation
 	const { customerId, amount, status } = CreateInvoice.parse({
 		customerId: formData.get("customerId"),
 		amount: formData.get("amount"),
 		status: formData.get("status"),
 	});
-	const amountInCents = amount * 100; // Store amount in cents to avoid floating numbers issues in DB
+
+	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split("T")[0];
 
-	await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+	try {
+		await sql`
+		  INSERT INTO invoices (customer_id, amount, status, date)
+		  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+		`;
+	} catch (error) {
+		return {
+			message: "Database Error: Failed to Create Invoice.",
+		};
+	}
 
-	// This clears the caché from specific path so it gets updated
-	revalidatePath("/dashboard/invoices"); // ! DEPRECATED
-	// Go back to invoices page
+	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
 }
 
@@ -53,17 +50,27 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 	const amountInCents = amount * 100;
 
-	await sql`
-	  UPDATE invoices
-	  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-	  WHERE id = ${id}
-	`;
+	try {
+		await sql`
+			 UPDATE invoices
+			 SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+			 WHERE id = ${id}
+		  `;
+	} catch (error) {
+		return { message: "Database Error: Failed to Update Invoice." };
+	}
 
-	revalidatePath("/dashboard/invoices"); // ! DEPRECATED
+	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-	await sql`DELETE FROM invoices WHERE id = ${id}`;
-	revalidatePath("/dashboard/invoices");
+	// throw new Error("Failed to Delete Invoice"); // ? Just for testing
+	try {
+		await sql`DELETE FROM invoices WHERE id = ${id}`;
+		revalidatePath("/dashboard/invoices");
+		return { message: "Deleted Invoice." };
+	} catch (error) {
+		return { message: "Database Error: Failed to Delete Invoice." };
+	}
 }
